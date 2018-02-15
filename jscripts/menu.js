0cfb25cfc186e4b507ac953ce40c21335e725da4
AR8Stefan <stefan.piatkov@gmail.com>
2018-02-15 12:10:50 -0600

    work on menu functions

--- jscripts/menu.js
+++ jscripts/menu.js
@@ -1,12 +1,51 @@
-var count = 0;
-var input = document.getElementById("count");
-  function plus(){
-    count++;
-    input.value = count;
-  }
-  function minus(){
-    if (count > 1) {
-        count--;
-        input.value = count;
-      }  
-    }
+// let count = 0;
+// let countEl = document.getElementById("count");
+function plus(){
+	countEl = document.getElementById("count")
+	count = count.value
+  count++;
+  countEl.value = count;
+}
+function minus(){
+  if (count > 0) {
+  	countEl = document.getElementById("count")
+	  count = count.value
+    count--;
+    countEl.value = count;
+  }  
+}
+
+// $(function() {
+
+//     $("form div").append('<div><input type="button" class="plus-btn plusbtn btn"></div><input type="text" value="" id="count"><div><input type="button" class="minus-btn btn">');
+
+// });
+
+// $(".button").on("click", function() {
+
+//   var $button = $(this);
+//   var oldValue = $button.parent().find("input").val(0);
+
+//   if ($button() == this ) {
+// 	  var newVal = parseFloat(oldValue) + 1;
+// 	} else {
+//    // Don't allow decrementing below zero
+//     if (oldValue > 0) {
+//       var newVal = parseFloat(oldValue) - 1;
+//     } else {
+//       newVal = 0;
+//     }
+//   }
+
+//   $button.parent().find("input").val(newVal);
+
+// });
+
+// var id = $button.attr("id");
+// $.ajax({
+//   type: "POST",
+//   url: "dosomething.php?id=" + id + "&newvalue=" + newVal,
+  // success: function() {
+  //   $button.parent().find("input").val(newVal);
+  
+// });
